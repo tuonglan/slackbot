c554a76172e0b17e54819c9b0092df6feae23488
Lan Do <tuonglan7777777@gmail.com>
2018-07-11 15:32:30 +0900

    Papago Translator class

---
+++ transbot/src/papago_trans.py
@@ -0,0 +1,32 @@
+import os, requests
+
+PAPAGO_API_URL = 'https://openapi.naver.com/v1/papago/n2mt'
+DEFAULT_CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=UTF-8'
+
+
+class PapagoTranslator:
+    def __init__(self, client_id, client_secret, source_lang='ko', target_lang='en'):
+        self._headers = {
+                'X-Naver-Client-Id': client_id,
+                'X-Naver-Client-Secret': client_secret,
+                'Content-Type': DEFAULT_CONTENT_TYPE
+            }
+        self._source_lang = source_lang
+        self._target_lang = target_lang
+
+    def translate(self, text, source_lang=None, target_lang=None):
+        s_lang = (source_lang or self._source_lang)
+        t_lang = (target_lang or self._target_lang)
+        payload = {'text': text, 'source': s_lang, 'target': t_lang}
+
+        res = requests.post(PAPAGO_API_URL, headers=self._headers, data=payload)
+        if res.status_code != 200:
+            raise Exception("Translation error: %s" % res)
+
+        js = res.json()
+        if not 'message' in js:
+            raise Exception("Translation error code: %s, message: %s" % (js['errorCode'], js['errorMessage']))
+        return js['message']['result']['translatedText']
+
+
+
---
+++ transbot/src/test/test-papago-trans.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import unittest, os, sys
+sys.path.insert(0, '../')
+from papago_trans import PapagoTranslator
+
+
+if __name__ == '__main__':
+    client_id = os.environ.get('PAPAGO_CLIENT_ID')
+    client_secret = os.environ.get('PAPAGO_CLIENT_SECRET')
+    trans = PapagoTranslator(client_id, client_secret)
+
+    text = trans.translate('앗; 이제 봤네요. 저희 도동집 왔는데 지금 음식이 나왔습니')
+    print "Translated text: \"%s\"" % text
