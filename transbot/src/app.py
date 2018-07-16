e1060254627694446fd071366aada82833e7639d
Lan Do <lan.do@nielsen.com>
2018-07-16 18:11:55 +0900

    Transbot: support channel mapping & message formate

--- transbot/src/app.py
+++ transbot/src/app.py
@@ -1,15 +1,19 @@
-import os, time
+import os, time, re
 from argparse import ArgumentParser
 
 from transbot import TranslatorBot
 
+CH_PAIR_PTT = re.compile('\(([\w|-]+,(?:\s)?[\w|-]+)\)')
+CH_PTT = re.compile('[\w|-]+')
 
 if __name__ == '__main__':
     # Get confiuration infos
     parser = ArgumentParser()
-    parser.add_argument('--username', type=str, 
+    parser.add_argument('--username', type=str, default=None,
                         help='Specify the username for slackbot to send private message to')
     parser.add_argument('--trans_delay', type=float, default=0.5, help='Delay of the bot between two scanning')
+    parser.add_argument('--channel_pairs', type=str, default=None, 
+                        help='Specify pairs of channels for translation')
 
     args = parser.parse_args()
 
@@ -17,9 +21,15 @@ if __name__ == '__main__':
     papago_client_id = os.environ.get('PAPAGO_CLIENT_ID')
     papago_client_secret = os.environ.get('PAPAGO_CLIENT_SECRET')
 
+    # Get the channel pairs
+    channel_pairs = None
+    if args.channel_pairs:
+        ch_list = CH_PAIR_PTT.findall(args.channel_pairs)
+        channel_pairs = [(x[0], x[1]) for x in [CH_PTT.findall(y) for y in ch_list]]
+
     # Initialization
-    bot = TranslatorBot(slack_bot_token, papago_client_id, papago_client_secret, args.username,
-                        rtm_read_delay=args.trans_delay)
+    bot = TranslatorBot(slack_bot_token, papago_client_id, papago_client_secret, username=args.username,
+                        rtm_read_delay=args.trans_delay, channel_pairs=channel_pairs)
 
     # Run and monitoring
     bot.start()
--- transbot/src/transbot.py
+++ transbot/src/transbot.py
@@ -7,12 +7,13 @@ from papago_trans import PapagoTranslator
 RTM_READ_DELAY = 0.5
 
 class TranslatorBot:
-    def __init__(self, token, papago_client_id, papago_client_secret, username, **kwargs):
+    def __init__(self, token, papago_client_id, papago_client_secret, **kwargs):
         self._client = SlackClient(token)
         if not self._client.rtm_connect():
             raise Exception("Can't establish Slack direct message connection")
         self._bot_id = self._client.api_call('auth.test')['user_id']
-        self._user_id = self._init_users(username)
+        self._user_id = self._init_users(kwargs.get('username', None))
+        self._trans_channels = self._init_translated_channels(kwargs.get('channel_pairs', None))
 
         self._rtm_read_delay = float(kwargs.get('rtm_read_delay', RTM_READ_DELAY))
         self.s_lang = kwargs.get('source_lang', 'ko')
@@ -29,19 +30,73 @@ class TranslatorBot:
         user_id = None
         self._users = {}
         for user in workspace_users['members']:
-            display_name = user['profile']['display_name'] == '' ? None :  user['profile']['display_name']
+            display_name = user['profile']['display_name']
+            display_name = None if display_name == '' else display_name
             self._users[user['id']] = {
                     'name': user['name'], 
                     'display_name': user['profile']['display_name']}
-            if user['name'] == username or user['profile']['display_name'] == username:
+            if username and (user['name'] == username or user['profile']['display_name'] == username):
                 user_id =  user['id']
        
-        if not user_id:
+        if username and (not user_id):
             raise Exception("Can't find user %s in the user list" % username)
         return user_id
 
+    def _init_translated_channels(self, channel_pairs):
+        if not channel_pairs:
+            return None
+
+        workspace_channels = self._client.api_call('channels.list')
+        if not workspace_channels['ok']:
+            raise Exception("Can't get channel list: %s" % workspace_channels['error'])
+        private_channels = self._client.api_call('groups.list')
+        if not private_channels['ok']:
+            raise Exception("Can't get private channel list: %s" % private_channels['error'])
+        
+        # Channel names -> id and id -> names
+        self._ch_names = {}
+        self._ch_ids = {}
+        for ch in workspace_channels['channels']:
+            self._ch_names[ch['name']] = ch['id']
+            self._ch_ids[ch['id']] = ch['name']
+        for ch in private_channels['groups']:
+            self._ch_names[ch['name']] = ch['id']
+            self._ch_ids[ch['id']] = ch['name']
+
+        # Create translated_channel_ids
+        trans_channels = {}
+        for (from_ch ,to_ch) in channel_pairs:
+            trans_channels[self._ch_names[from_ch]] = self._ch_names[to_ch]
+
+        return trans_channels
+
     def _handle_command(self, event):
         print "-----> Command: %s" % event['text']
+
+    def _post_ephemeral(self, trans_text, user_id, event):
+        display_name = self._users[event['user']]['display_name'] or self._users[event['user']]['name']
+        atts = [{'pretext': "%s said: \"%s...\"" % (display_name, event['text'][:23]),
+                'text': trans_text['text'],
+                'color': "#7CD197"
+                }]
+        res = self._client.api_call('chat.postEphemeral', attachments=atts, user=user_id, 
+                                    channel=event['channel'])
+        if not res['ok']:
+            print "Can't post message: %s to the slack channel %s" % (trans_text['text'], event['channel'])
+
+    def _post_channel_message(self, trans_text, channel_id, event):
+        display_name = self._users[event['user']]['display_name'] or self._users[event['user']]['name']
+        atts = [{'pretext': "*'%s'* (<#%s>): \"%s...\"" % (display_name, event['channel'],
+                                                           event['text'][:23]),
+                'text': trans_text['text'],
+                'color': "#7CD197"
+                }]
+        res = self._client.api_call('chat.postMessage', attachments=atts,
+                                    channel=self._trans_channels[event['channel']])
+        if not res['ok']:
+            print "Can't post message: %s to the slack channel %s" % (trans_text['text'], 
+                                                                      self._trans_channels[event['channel']])
+
 
     def _handle_event(self, event):
         # If it's not a message or include 'subtype', ignore
@@ -61,14 +116,13 @@ class TranslatorBot:
                                                                                self.t_lang, e)
             trans_text = {'text': "Error at translation: %s" % e.message}
         
-        atts = [{'pretext': "%s said" % event['user'],
-                'text': trans_text['text'],
-                'color': "#7CD197"
-                }]
-        res = self._client.api_call('chat.postEphemeral', attachments=atts, user=self._user_id, 
-                                    channel=event['channel'])
-        if not res['ok']:
-            print "Can't post message: %s to the slack channel %s" % (trans_text['text'], event['channel'])
+        # Post ephemeral message if user id specified
+        if self._user_id:
+            self._post_ephemeral(trans_text, self._user_id, event)
+
+        # Post translated message in the channel if configured
+        if self._trans_channels and event['channel'] in self._trans_channels:
+            self._post_channel_message(trans_text, event['channel'], event)
 
     def _start(self):
         while self._running:
@@ -76,7 +130,7 @@ class TranslatorBot:
                 events = self._client.rtm_read()
                 map(self._handle_event, events)
             except Exception as e:
-                print "Translator ERROR: %s" % e
+                print "Translator Bot ERROR: %s" % e
 
             time.sleep(self._rtm_read_delay)
 
